fd6fd11455425b03c1909c31f9b16ec6e055d5cc
Veniel Dorestave <veniel@noukod.com>
2019-04-01 15:14:13 +0000

    Automatically backed up by Learn

--- src/reducers/manageAuthorsAndBooks.js
+++ src/reducers/manageAuthorsAndBooks.js
@@ -1,34 +1,34 @@
 import { combineReducers } from "redux";
 import uuid from "uuid";
- 
+
 const rootReducer = combineReducers({
   authors: authorsReducer,
   books: booksReducer
 });
- 
+
 export default rootReducer;
- 
+
 function booksReducer(state = [], action) {
   let idx;
   switch (action.type) {
     case "ADD_BOOK":
       return [...state, action.book];
- 
+
     case "REMOVE_BOOK":
       idx = state.indexOf(action.id);
       return [...state.slice(0, idx), ...state.slice(idx + 1)];
- 
+
     default:
       return state;
   }
 }
- 
+
 function authorsReducer(state = [], action) {
   let idx;
   switch (action.type) {
     case "ADD_AUTHOR":
       return [...state, action.author];
- 
+
     case "REMOVE_AUTHOR":
       idx = state.indexOf(action.id);
       return [...state.slice(0, idx), ...state.slice(idx + 1)];
@@ -42,7 +42,7 @@ function authorsReducer(state = [], action) {
     } else {
       return [...state, { authorName: action.book.authorName, id: uuid() }];
     }
- 
+
     default:
       return state;
   }
